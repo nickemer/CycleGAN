fb0a1b37304eb7532cc48fe6587a7de48ddce9d1
Nicholas Emer <nicholasemer@Nicholass-MacBook-Pro.local>
2025-03-23 18:08:24 -0600

    typo fixed

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -112,11 +112,11 @@ class CycleGANModel(BaseModel):
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.fake_B = self.netG_A(self.real_A)  # G_A(A)
-        self.noise_fake_B = self.fake_B + torch.radn_like(self.fake_B) * self.opt.noise_std # adds noise to fakeB with noise_std option
+        self.noise_fake_B = self.fake_B + torch.randn_like(self.fake_B) * self.opt.noise_std # adds noise to fakeB with noise_std option
         self.rec_A = self.netG_B(self.fake_B)   # G_B(G_A(A))
 
         self.fake_A = self.netG_B(self.real_B)  # G_B(B)
-        self.noise_fakeA = self.fake_A + torch.radn_like(self.fake_A) * self.opt.noise_std # adds noise to fakeA with noise_std option
+        self.noise_fakeA = self.fake_A + torch.randn_like(self.fake_A) * self.opt.noise_std # adds noise to fakeA with noise_std option
         self.rec_B = self.netG_A(self.fake_A)   # G_A(G_B(B))
 
     def backward_D_basic(self, netD, real, fake):
